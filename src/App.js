f6df809e4415ff833e9766e0867ba3d734be4bf6
gregmarquet <marquet.greg@gmail.com>
2018-10-20 17:01:02 -0400

    add input of type range in Quiz and add handleChange in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import InputRange from 'react-input-range';
+
 import Quiz from './components/Quiz/Quiz';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: 10}
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value
+    this.setState({
+      value: value
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +26,10 @@ class App extends Component {
           <h2>The app that will help you grow!</h2>
         </header>
         <div>
-          <Quiz />
+          
+          <Quiz 
+            value={this.state.value}
+            onValueChange={this.handleChange}/>
         </div>
       </div>
     );
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import InputRange from 'react-input-range';
 
 
 function Quiz(props) {
   return (
     <div>
       Here is the Quiz!
+      <input 
+        type="range" 
+        min="0" 
+        max="10" 
+        value={props.value}
+        onChange={props.onValueChange}/>
+        {props.value}
     </div>
   )
 }
