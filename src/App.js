f987278c5ddfb653a542e5fce3b4118088828756
gregmarquet <marquet.greg@gmail.com>
2018-10-21 16:22:31 -0400

    add separation and area to Quiz

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { value: 10}
+    this.state = { value: 0}
   }
 
   handleChange = (event) => {
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,14 +7,17 @@ import './Quiz.css'
 function Quiz(props) {
   return (
     <div className="container">
+      <div className="separation"></div>
       <p className="intro-text">Here are some areas of improvement, rate them from 0 to 10 based on how happy or unhappy you are in those areas compared to where you would like to be. </p>
+      <div className="separation"></div>
+      <p className="area">Love: {props.value}</p>
       <input 
         type="range" 
         min="0" 
         max="10" 
         value={props.value}
         onChange={props.onValueChange}/>
-        {props.value}
+      
     </div>
   )
 }
