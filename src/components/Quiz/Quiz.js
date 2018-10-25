062b52e5b3d6e9e7100e18067490340c39e34ffa
gregmarquet <marquet.greg@gmail.com>
2018-10-25 12:01:34 -0400

    change handleChange in Quiz.js and add second input

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -8,14 +8,21 @@ class Quiz extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { value: 0 }
+    this.state = { 
+      values: {
+        love: 0,
+        finance: 0
+      }
+    }
   }
 
   handleChange = (event) => {
+    const values = this.state.values
     const value = event.target.value
-    this.setState({
-      value: value
-    })
+    const name = event.target.name
+    values[name] = value
+    console.log(name, value)
+    this.setState({ values })
   }
   render() {
     return (
@@ -23,12 +30,22 @@ class Quiz extends Component {
         <div className="separation"></div>
         <p className="intro-text">Here are some areas of improvement, rate them from 0 to 10 based on how happy or unhappy you are in those areas compared to where you would like to be. </p>
         <div className="separation"></div>
-        <p className="area">Love: {this.state.value}</p>
+        <p className="area">Love: {this.state.values.love}</p>
         <input
           type="range"
           min="0"
           max="10"
-          value={this.state.value}
+          name="love"
+          value={this.state.values.love}
+          onChange={this.handleChange} />
+        
+        <p className="area">Finance: {this.state.values.finance}</p>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          name="finance"
+          value={this.state.values.finance}
           onChange={this.handleChange} />
 
       </div>
