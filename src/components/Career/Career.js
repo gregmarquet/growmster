d63edcf19637cde21b4745597c789cc647c2286b
gregmarquet <marquet.greg@gmail.com>
2018-10-29 19:07:08 -0400

    add Career.js and Career.css

---
+++ src/components/Career/Career.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Career.css'
+
+function Career (props) {
+  return (
+    <div className="area-container">
+      <h3 className="area_title">Career</h3>
+      <a target="_blank" href="https://www.amazon.com/gp/product/0143129341/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=0143129341&linkCode=as2&tag=gregmarquet-20&linkId=6d747f0e59f019f4cad3206f5a5dfdb7"><img border={0} src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=0143129341&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=gregmarquet-20" /></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=gregmarquet-20&l=am2&o=1&a=0143129341" width={1} height={1} border={0} alt={'The Career Manifesto'} style={{ border: 'none !important'}} />
+
+      <a target="_blank" href="https://www.amazon.com/gp/product/1451639619/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1451639619&linkCode=as2&tag=gregmarquet-20&linkId=66c4f63fae1f5f18860be66a8d7ffaa6"><img border={0} src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=US&ASIN=1451639619&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL160_&tag=gregmarquet-20" /></a><img src="//ir-na.amazon-adsystem.com/e/ir?t=gregmarquet-20&l=am2&o=1&a=1451639619" width={1} height={1} border={0} alt={'The 7 Habits of Highly Effective People'} style={{ border: 'none !important'}} />
+    </div>
+  )
+}
+
+export default Career;
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Chart from '../Chart/Chart'
+import Career from '../Career/Career'
 
 function Result(props) {
   return (
     <div>
       <Chart 
         values={props.values}/>
+      <Career />
     </div>
   )
 }
